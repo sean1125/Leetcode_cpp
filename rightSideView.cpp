dedae1b8e2a8da423651c3650572b3a41c0669ca
Sean <shixiaoran1989@gmail.com>
2015-04-05 12:36:08 -0700

    Create rightSideView.cpp

---
+++ rightSideView.cpp
@@ -0,0 +1,49 @@
+// 199 Binary Tree Right Side View
+
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode *root) {
+        vector<int> result;
+        
+        if (root == NULL)
+            return result;
+            
+        int counter = 0, remain = 1;
+        queue<TreeNode *> Q;
+        Q.push(root);
+        TreeNode * tmp;
+        
+        while (Q.empty() != true) {
+            tmp = Q.front();
+            
+            if (tmp->left != NULL) {
+                Q.push(tmp->left);
+                counter++;
+            }
+            if (tmp->right != NULL) {
+                Q.push(tmp->right);
+                counter++;
+            }
+            
+            Q.pop();
+            remain--;
+            
+            if(remain == 0) {
+                remain = counter;
+                counter = 0;
+                result.push_back(tmp->val);
+            }
+        }
+        
+        return result;
+    }
+};
