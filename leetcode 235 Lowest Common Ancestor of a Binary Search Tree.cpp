44ccd9ca996e94d9b5918844b9841403eb14fb8b
Sean <shixiaoran1989@gmail.com>
2015-07-10 19:27:31 -0700

    Update leetcode 235 Lowest Common Ancestor of a Binary Search Tree.cpp

--- leetcode 235 Lowest Common Ancestor of a Binary Search Tree.cpp
+++ leetcode 235 Lowest Common Ancestor of a Binary Search Tree.cpp
@@ -14,28 +14,12 @@ public:
         if (p->val < root->val && root->val < q->val) return root;
         if (p->val > root->val) return LCA(root->right, p, q);
         if (root->val > q->val) return LCA(root->left, p, q);
-
-        if (p->val == root->val) return root;
-        if (q->val == root->val) return root;
-    }
-
-    TreeNode * findRoot(TreeNode * p, TreeNode * q) {
-
-        TreeNode * tmp = p;
-        
-        while (tmp && tmp->val == q->val) {
-            if (tmp == q) return p;
-            tmp = tmp->right;
-        }
-
-        return q;
+        if (p->val == root->val || q->val == root->val) return root;
     }
 
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
 
         if (!root || !p || !q) return NULL;
-
-        if (p->val == q->val) return findRoot(p, q);
 
         if (p->val < q->val) return LCA(root, p, q);
         else  return LCA(root, q, p);
