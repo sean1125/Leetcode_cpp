965efce69ebdce4e3695595d97744f523e645dd4
Sean <shixiaoran1989@gmail.com>
2015-07-20 21:23:50 -0700

    Create leetcode 239 Sliding Window Maximum.cpp

---
+++ leetcode 239 Sliding Window Maximum.cpp
@@ -0,0 +1,26 @@
+// leetcode 239 Sliding Window Maximum.cpp
+
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+
+        int i, size = nums.size();        
+        vector<int> result;
+        map<int, int> m;
+
+        for (i = 0; i < k; i++) {
+            m[nums[i]]++;
+        }
+
+        for (; i < size; i++) {
+            result.push_back(m.rbegin()->first);
+            m[nums[i - k]]--;
+            if (!m[nums[i - k]]) m.erase(nums[i - k]);
+            m[nums[i]]++;
+        }
+
+        if (size) result.push_back(m.rbegin()->first);
+
+        return result;
+    }
+};
