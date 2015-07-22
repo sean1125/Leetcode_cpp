60447100fb9a0f7fb98a801977dbf176a6b2d6f0
Sean <shixiaoran1989@gmail.com>
2015-07-21 18:01:14 -0700

    Update leetcode 239 Sliding Window Maximum.cpp

--- leetcode 239 Sliding Window Maximum.cpp
+++ leetcode 239 Sliding Window Maximum.cpp
@@ -1,5 +1,35 @@
 // leetcode 239 Sliding Window Maximum.cpp
 
+// This is the optimal solution, the time complexity is O(n).
+
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+
+        int i, size= nums.size();
+        vector<int> result;
+        deque<int> q;
+
+        for (i = 0; i < k; i++) {
+            while (!q.empty() && nums[q.back()] <= nums[i]) q.pop_back();
+            q.push_back(i);
+        }
+
+        for (; i < size; i++) {
+            result.push_back(nums[q.front()]);
+
+            while (!q.empty() && nums[q.back()] <= nums[i]) q.pop_back();
+            q.push_back(i);
+
+            while (q.front() <= i - k) q.pop_front();
+        }
+
+        if (size) result.push_back(nums[q.front()]);
+
+        return result;
+    }
+};
+
 // This is NOT the optimal solution, time complexity is O[n log(k)].
 
 class Solution {
