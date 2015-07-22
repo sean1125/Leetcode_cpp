ae8b87d18dc5a43bbf39bcbe06c02d1488f28314
Sean <shixiaoran1989@gmail.com>
2015-07-22 15:47:00 -0700

    Update leetcode 240 Search a 2D Matrix II.cpp

--- leetcode 240 Search a 2D Matrix II.cpp
+++ leetcode 240 Search a 2D Matrix II.cpp
@@ -6,25 +6,21 @@ class Solution {
 public:
     bool searchMatrix(vector<vector<int> > & m, int t, int ys, int ye, int xs, int xe) {
 
-        if (ys > ye || xs > xe) return false;
+        if (ys > ye || xs > xe || t < m[ys][xs] || t > m[ye][xe]) return false;
 
         int ym = (ys + ye) / 2, xm = (xs + xe) / 2;
+        int mid = m[ym][xm];
 
-        if (m[ym][xm] == t) return true;
-        if (m[ym][xm] < t) return searchMatrix(m, t, ym + 1, ye, xs, xe) || searchMatrix(m, t, ys, ym, xm + 1, xe);
-        if (m[ym][xm] > t) return searchMatrix(m, t, ys, ym - 1, xs, xe) || searchMatrix(m, t, ym, ye, xs, xm - 1);
+        if (mid == t) return true;
+        if (mid < t) return searchMatrix(m, t, ym + 1, ye, xs, xe) || searchMatrix(m, t, ys, ym, xm + 1, xe);
+        if (mid > t) return searchMatrix(m, t, ys, ym - 1, xs, xe) || searchMatrix(m, t, ym, ye, xs, xm - 1);
     }
 
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
 
-        if (matrix.size() == 0) return false;
-
-        int sizeY = matrix.size(), sizeX = matrix[0].size();
-
-        return searchMatrix(matrix, target, 0, sizeY - 1, 0, sizeX - 1);
+        return !matrix.empty() && searchMatrix(matrix, target, 0, matrix.size() - 1, 0, matrix[0].size() - 1);
     }
 };
-
 
 // solution 2
 
