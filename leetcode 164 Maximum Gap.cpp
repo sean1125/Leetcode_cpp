82477bea3ce2e8e05487c5973dfb5d3b13c13f81
Sean <shixiaoran1989@gmail.com>
2015-08-02 13:39:21 -0700

    Update leetcode 164 Maximum Gap.cpp

--- leetcode 164 Maximum Gap.cpp
+++ leetcode 164 Maximum Gap.cpp
@@ -4,46 +4,46 @@ class Solution {
 public:
     void countingSort(vector<int> & nums, int m) {
 
-        int i, j, size = nums.size();
+        int i, size = nums.size();
         vector<int> counter(10, 0);
         vector<int> sorted(size);
 
-        for (i = 0; i < size; i++)
-            counter[nums[i] / m % 10]++;
+        for (i = 0; i < size; i++) counter[nums[i] / m % 10]++;
 
-        for (j = 1; j < 10; j++)
-            counter[j] += counter[j - 1];
+        for (i = 1; i < 10; i++) counter[i] += counter[i - 1];
 
         for (i = size - 1; i >= 0; i--) {
-            sorted[counter[nums[i] / m % 10] - 1] = nums[i];
-            counter[nums[i] / m % 10]--;
+            int tmp = nums[i] / m % 10;
+            sorted[counter[tmp] - 1] = nums[i];
+            counter[tmp]--;
         }
 
         nums = sorted;
+
+        return;
     }
 
-    void sort(vector<int> & nums) {
+    void radixSort(vector<int> & nums) {
 
         int m, i, size = nums.size(), max = 0;
 
-        for (i = 0; i < size; i++)
-            max = max < nums[i] ? nums[i] : max;
+        for (i = 0; i < size; i++) max = max > nums[i] ? max : nums[i];
 
-        for (m = 1; m <= max && m <= 1000000000; m *= 10)
-            countingSort(nums, m);
+        for (m = 1; m <= max && m <= 1000000000; m *= 10) countingSort(nums, m);
+
+        return;
     }
 
     int maximumGap(vector<int>& nums) {
 
-        int size = nums.size(), i;
-        if (size < 2) return 0;
+        int size = nums.size(), i, max = 0;
 
-        sort(nums);
+        radixSort(nums);
 
-        int max = 0;
-
-        for (i = 0; i < size - 1; i++)
-            max = max < (nums[i + 1] - nums[i]) ? (nums[i + 1] - nums[i]) : max;
+        for (i = 0; i < size - 1; i++) {
+            int tmp = nums[i + 1] - nums[i];
+            max = max > tmp ? max : tmp;
+        }
 
         return max;
     }
