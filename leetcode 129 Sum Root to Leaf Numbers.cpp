4336028f5a6410bf7dde93a620c8a4b01107eca3
Sean <shixiaoran1989@gmail.com>
2015-08-04 19:33:06 -0700

    Update leetcode 129 Sum Root to Leaf Numbers.cpp

--- leetcode 129 Sum Root to Leaf Numbers.cpp
+++ leetcode 129 Sum Root to Leaf Numbers.cpp
@@ -1,7 +1,5 @@
-// leetcode 129 Sum Root to Leaf Numbers
-
 /**
- * Definition for binary tree
+ * Definition for a binary tree node.
  * struct TreeNode {
  *     int val;
  *     TreeNode *left;
@@ -11,25 +9,26 @@
  */
 class Solution {
 public:
-    void sumNumbers(TreeNode * curr, int number, vector<int> & array) {
-        number *= 10;
-        number += curr->val;
-        
-        if (curr->left) sumNumbers(curr->left, number, array);
-        if (curr->right) sumNumbers(curr->right, number, array);
-        if (!curr->left && !curr->right) array.push_back(number);
+    void sumNumbers(TreeNode * root, int num, int & sum) {
+
+        num *=10;
+        num += root->val;
+
+        if (root->left) sumNumbers(root->left, num, sum);
+        if (root->right) sumNumbers(root->right, num, sum);
+        if (!root->left && !root->right) sum += num;
+
+        return;
     }
 
-    int sumNumbers(TreeNode *root) {
+    int sumNumbers(TreeNode* root) {
+
         if (!root) return 0;
-        
-        vector<int> array;
-        sumNumbers(root, 0, array);
-        int i, counter = 0, size = array.size();
-        
-        for (i = 0; i < size; i++)
-            counter += array[i];
-            
-        return counter;
+
+        int sum = 0;
+
+        sumNumbers(root, 0, sum);
+
+        return sum;
     }
 };
