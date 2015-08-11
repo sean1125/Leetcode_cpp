ab14768f22d1d259ca2e2c3862240dd0ebb284ea
Sean <shixiaoran1989@gmail.com>
2015-08-11 13:23:30 -0700

    Update leetcode  96 Unique Binary Search Trees.cpp

--- leetcode  96 Unique Binary Search Trees.cpp
+++ leetcode  96 Unique Binary Search Trees.cpp
@@ -3,21 +3,24 @@
 class Solution {
 public:
     int numTrees(int start, int end, vector<int> & array) {
-        if (start >= end) return 1;
+
+        if (start > end) return 1;
         if (array[end - start] > 0) return array[end - start];
 
         int counter = 0, i;
-        
+
         for (i = start; i <= end; i++)
             counter += (numTrees(start, i - 1, array) * numTrees(i + 1, end, array));
 
         array[end - start] = counter;
-        return counter;
+
+        return array[end - start];
     }
 
     int numTrees(int n) {
+
         vector<int> array(n, -1);
-        
+
         return numTrees(1, n, array);
     }
 };
