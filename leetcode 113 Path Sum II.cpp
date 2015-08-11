f22358743492f3bc65b7a75312e84f06b804636b
Sean <shixiaoran1989@gmail.com>
2015-08-11 12:51:26 -0700

    Update leetcode 113 Path Sum II.cpp

--- leetcode 113 Path Sum II.cpp
+++ leetcode 113 Path Sum II.cpp
@@ -12,23 +12,29 @@
 class Solution {
 public:
     void pathSum(TreeNode * curr, int sum, vector<vector<int> > & result, vector<int> & array) {
-        array.push_back(curr->val);
+
         sum -= curr->val;
-        
+
+        array.push_back(curr->val);
+
         if (curr->left) pathSum(curr->left, sum, result, array);
         if (curr->right) pathSum(curr->right,sum, result, array);
-        if (!curr->left && !curr->right && 0 == sum) result.push_back(array);
+        if (!curr->left && !curr->right && !sum) result.push_back(array);
 
         array.pop_back();
+
+        return;
     }
 
     vector<vector<int> > pathSum(TreeNode *root, int sum) {
+
         vector<vector<int> > result;
         vector<int> array;
-        
+
         if (!root) return result;
-        
+
         pathSum(root, sum, result, array);
+
         return result;
     }
 };
