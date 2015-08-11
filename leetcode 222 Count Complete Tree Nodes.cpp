a25b3de0196fe28a035f3211a269cbdb3d8ed607
Sean <shixiaoran1989@gmail.com>
2015-08-11 13:46:37 -0700

    Update leetcode 222 Count Complete Tree Nodes.cpp

--- leetcode 222 Count Complete Tree Nodes.cpp
+++ leetcode 222 Count Complete Tree Nodes.cpp
@@ -13,21 +13,19 @@ class Solution {
 public:
     int countNodes(TreeNode* root) {
 
-        if (!root) return 0;
-
-        int lh, rh, num = 1;
         TreeNode * left = root, * right = root;
+        int i, counter = 1;
 
-        for (lh = 0; left; lh++)
+        for (i = 0; left && right; i++) {
             left = left->left;
-        for (rh = 0; right; rh++)
             right = right->right;
-
-        if (lh == rh) {
-            while (lh--) num *= 2;
-            return --num;
         }
 
-        return countNodes(root->left) + countNodes(root->right) + 1;
+        if (!left && !right) {
+            while (i--) counter *= 2;
+            return counter - 1;
+        } else {
+            return countNodes(root->left) + countNodes(root->right) + 1;
+        }
     }
 };
