1ad08930f40019e6e1f8adbe5a828b1f26013ac8
Sean <shixiaoran1989@gmail.com>
2015-08-16 02:10:41 -0700

    Update leetcode 258 Add Digits.cpp

--- leetcode 258 Add Digits.cpp
+++ leetcode 258 Add Digits.cpp
@@ -4,7 +4,6 @@ class Solution {
 public:
     int addDigits(int num) {
 
-        if (!num) return 0;
-        else return (num % 9) ? (num % 9) : 9;
+        return num ? ((num % 9) ? (num % 9) : 9) : 0;
     }
 };
